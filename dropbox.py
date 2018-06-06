545b1fb8ade1c04c947a3ac2ad42523ec6916b12
ProgrammingKiddo <programming.edu@protonmail.com>
2018-06-06 22:07:48 +0200

    Implementado el script de subida de ficheros a DB.

--- dropbox.py
+++ dropbox.py
@@ -1,76 +1,57 @@
 import pika
-import dropbox
-from dropbox.files import WriteMode
-from dropbox.exceptions import ApiError, AuthError
+import sys
 import os, time
+import dropbox
 
 
 
 TOKEN = 'zaOSmPB-YZAAAAAAAAAABs6lSuUqn7vXbcEfSZjZeNkTyrOMDGgjnV8rjcegmPlZ'
-LOCALFILE = "file.dat"
-UPLOAD_PATH = "/" + LOCALFILE
 
 
-def publish():
+def publish(self, msg):
 
-    with open(LOCALFILE, "rb") as f:
-        print("\n\nSubiendo " + LOCALFILE + " a Dropbox...")
+    print("Conectando con Dropbox...")
+    dbx = dropbox.Dropbox(TOKEN)
+    user = dbx.users_get_current_account()
+    print("Usuario " + user.name.display_name + " de " + user.country)
 
-        try:
-            dbx.files_upload(f.read(), UPLOAD_PATH, mode=WriteMode('overwrite'))
-            print("hi")
-        except ApiError as err:
-            # This checks for the specific error where a user doesn't have
-            # enough Dropbox space quota to upload this file
-            if (err.error.is_path() and
-                    err.error.get_path().reason.is_insufficient_space()):
-                sys.exit("ERROR: Cannot back up; insufficient space.")
-            elif err.user_message_text:
-                print(err.user_message_text)
-                sys.exit()
-            else:
-                print(err)
-                sys.exit()
+    print("\n\nSubiendo diagrama a Dropbox...")
+
+    try:
+        nombre = [line.split(":")[0]]
+        fichero = "/" + nombre + "png"
+        dbx.files_upload(msg, fichero, mode=dropbox.files.WriteMode('overwrite'))
+        print("hi")
+    except dropbox.exceptions.ApiError as err:
+        if (err.error.is_path() and
+                err.error.get_path().reason.is_insufficient_space()):
+            sys.exit("ERROR: Cannot back up; insufficient space.")
+        elif err.user_message_text:
+            print(err.user_message_text)
+            sys.exit()
+        else:
+            print(err)
+            sys.exit()
+
+    metadata = dropbox.sharing.GetFileMetadataIndividualResult(fichero)
+    finalURL = metadata.preview_url
+    channel.basic_publish(exchange='', routing_key='linkDropbox', body=finalURL)
+
+
+
+def callback(ch, method, properties, body) :
+    self.publish(body)
+
 
 
 url = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@localhost:5672/%2f')
 params = pika.URLParameters(url)
 connection = pika.BlockingConnection(params)
+
 channel = connection.channel()
 channel.queue_declare(queue='graficosRelacion') # Cola de lectura
 channel.queue_declare(queue='linkDropbox') # Cola de escritura
 
-
-print("Conectando con Dropbox...")
-dbx = dropbox.Dropbox(TOKEN)
-user = dbx.users_get_current_account()
-print("Usuario " + user.name.display_name + " de " + user.country)
-
-publish()
-print("Listo!")
-
-
-
-
-
-
-
-
-
-
-
-
-# Access the CLODUAMQP_URL environment variable and parse it (fallback to localhost)
-
-# create a function which is called on incoming messages
-def callback(ch, method, properties, body):
-  pdf_process_function(body)
-
-# set up subscription on the queue
-channel.basic_consume(callback,
-  queue='pdfprocess',
-  no_ack=True)
-
-# start consuming (blocks)
+channel.basic_consume(callback, queue='graficosRelacion', no_ack=True)
 channel.start_consuming()
 connection.close()
